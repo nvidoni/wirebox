678b7047b1512286b1a3cc1ebdad03eb47c1cd65
Nikola Vidoni <nikola.vidoni@net.amis.hr>
2013-04-11 14:20:44 +0200

    Opimized CSS, JS (removed unnecessary stuff), added configurable options to the module

--- Wirebox.js
+++ Wirebox.js
@@ -4,7 +4,7 @@
  * Author: Nicolas Turlais, June 2012, nicolas-at-insipi.de
  * Modified: Nikola Vidoni, April 2013
  * 
- * Version: 0.3.1
+ * Version: 1.0.5
  * 
  * Licensed under CC Attribution-ShareAlike
  *
@@ -15,113 +15,83 @@
 	var calls = 0;
 	$.fn.wirebox = function(settings) {
 		settings = $.extend({
-			container:					$('body'),
-			displayAsLink:				false,
-			linkImages:					true,
-			linksContainer:				'wireboxLinksContainer',				
-			overlayOpacity:				0.9,
-			overlayColor:				'#000',
-			overlayClose:				true,
-			fadeInOverlayduration:		200,
-			fadeInImageduration:		200,
-			fadeOutImageduration:		200,		
-			name: 						"Image ",
-			separator: 					" / ",
+			container:					$('body'),				
+			overlayOpacity:				WireboxInput.overlayOpacity,
+			overlayColor:				WireboxInput.overlayColor,
+			overlayClose:				WireboxInput.overlayClose,
+			fadeInOverlayDuration:		WireboxInput.fadeInOverlayDuration,
+			fadeOutOverlayDuration:		WireboxInput.fadeOutOverlayDuration,
+			fadeInImageDuration:		WireboxInput.fadeInImageDuration,
+			fadeOutImageDuration:		WireboxInput.fadeOutImageDuration,
+			prefix: 					WireboxInput.prefix,
+			separator: 					WireboxInput.separator,
 			currentImage:				0,
 			setIndex:					0,
-			setTitle:					'',
 			lastImage:					0
-		},settings);
+		}, settings);
 		
 		calls++;
 		settings.setIndex = calls;
 		images[settings.setIndex] = [];
 		
 		//images:
-		this.each(function(index){
-			if(index == 0 && settings.linkImages && settings.setTitle == ''){
-				settings.setTitle = isSet($(this).attr('rel'), ' ');
-			}
+		this.each(function(index) {
 			$(this).each(function() {
-				images[settings.setIndex]['displayAsLink'] = settings.displayAsLink;
 				images[settings.setIndex][index] = [];
 				images[settings.setIndex][index]['location'] = isSet($(this).attr('href'), ' ');
 				images[settings.setIndex][index]['caption'] = isSet($(this).attr('title'), ' ');
-				if(!settings.displayAsLink){
-					$(this).unbind('click').bind('click', {id: settings.setIndex, nom : settings.setTitle, i : index}, _initialise);
-				}
+					$(this).bind('click', {id: settings.setIndex, i : index}, _initialize);
 			})
 		});
-		
-		//setIndex:
-		for(var i = 0; i < images[settings.setIndex].length; i++)
-		{
-			if(images[settings.setIndex]['displayAsLink']){
-				if($('#'+settings.linksContainer).size() == 0){
-					this.filter(":first").before('<ul id="'+settings.linksContainer+'"></ul>');
-				}
-				$('#'+settings.linksContainer).append('<li><a href="#" id="wireboxNumSetIndex_'+settings.setIndex+'" class="wireboxLink">'+settings.setTitle+'</a></li>');
-				e = this.parent();
-				$(this).remove();
-				if($.trim(e.html()) == ""){//If parent empty : remove it
-					e.remove();
-				}
-				return $('#wireboxNumSetIndex_'+settings.setIndex).unbind('click').bind('click', {id: settings.setIndex, nom : settings.setTitle, i : settings.currentImage}, _initialise);
-			}
-		}
-		
-		function _initialise(event) {
-			
+
+		function _initialize(event) {
 			settings.currentImage = event.data.i;
 			settings.setIndex = event.data.id;
-			settings.setTitle = event.data.nom;
 			settings.lastImage = images[settings.setIndex].length - 1;
 			showWirebox();
 			return false;
 		}
-		function _interface(){
-			//html
+		function _interface() {
 			clear();
-			settings.container.append('<div id="wireboxOverlay"></div><div id="wireboxContent"><div id="wireboxClose"></div><div id="wireboxLoading"></div><div id="wireboxContainer"><img id="wireboxImage" src="" /></div><div id="wireboxDescription"><span id="wireboxTitle"></span><span id="wireboxCounter"></span></div><div id="wireboxLeft" class="wireboxArrows"></div><div id="wireboxRight" class="wireboxArrows"></div></div>');
-			if(settings.container.get(0).nodeName.toLowerCase() !== 'body'){
+			settings.container.append('<div id="wireboxOverlay"></div><div id="wireboxContent"><div id="wireboxTitle"></div><div id="wireboxClose"></div><div id="wireboxLoading"></div><div id="wireboxContainer"><img id="wireboxImage" src="" /></div><div id="wireboxCounter"></div><div id="wireboxLeft" class="wireboxArrows"></div><div id="wireboxRight" class="wireboxArrows"></div></div>');
+			if (settings.container.get(0).nodeName.toLowerCase() !== 'body') {
 				settings.container.css({'position':'relative','overflow':'hidden','line-height':'normal'});
 				$('#wireboxContent').css('position','relative');
 				$('#wireboxOverlay').css('position', 'absolute');
 			}
-			//events
-			$(document).unbind('keydown').bind('keydown', function(e){
-				switch(e.keyCode){
+			$(document).unbind('keydown').bind('keydown', function(e) {
+				switch(e.keyCode) {
 					case 37:
-						changePageWirebox(-1);
+						changePage(-1);
 						break;
 					case 39:
-						changePageWirebox(1);
+						changePage(1);
 						break;
 					case 27:
 						close();
 						break;
 				};
 			});
-			if(settings.overlayClose){
-				$('#wireboxOverlay').click(function(){
+			if (settings.overlayClose) {
+				$('#wireboxOverlay').click(function() {
 					close();
 					return false;
 				});
 			}
-			$('#wireboxLeft').unbind().bind('click', function(){
-				changePageWirebox(-1);
+			$('#wireboxLeft').unbind().bind('click', function() {
+				changePage(-1);
 				return false;
 			});
-			$('#wireboxRight').unbind().bind('click', function(){
-				changePageWirebox(1);
+			$('#wireboxRight').unbind().bind('click', function() {
+				changePage(1);
 				return false;
 			});
-			$('#wireboxClose').unbind().bind('click', function(){
+			$('#wireboxClose').unbind().bind('click', function() {
 				close();
 				return false;
 			});
 			$(window).resize(function() {
-				load(settings.currentImage,true);
+				load(settings.currentImage, true);
 			});
 	
 		}
@@ -132,146 +102,139 @@
 			$('#wireboxContent').fadeIn(settings.fadeInImageduration,function(){});
 			
 		}
-		function load(image,resize){
+		function load(image,resize) {
 			settings.currentImage = image;
 			$('#wireboxLoading').fadeIn(settings.fadeInImageduration);
 			var imgPreloader = new Image();
-			imgPreloader.onload = function(){
+			imgPreloader.onload = function() {
 				$('#wireboxImage').attr('src',images[settings.setIndex][settings.currentImage]['location']);
-				var adjust = iWantThePerfectImageSize(imgPreloader.height,imgPreloader.width);
+				var adjust = resizeImage(imgPreloader.height,imgPreloader.width);
 				Wirebox(adjust['height'],adjust['width'],resize);
 				$('#wireboxLoading').stop().fadeOut(settings.fadeOutImageduration);
 			};
 			imgPreloader.src = images[settings.setIndex][settings.currentImage]['location'];
 			preload();
-			upadteDescription();
+			description();
 		}
-		function changePageWirebox(signe){
-			if(!settings.linkImages || (settings.currentImage == 0 && signe == -1) || (settings.currentImage == settings.lastImage && signe == 1))
-			{
+		function changePage(index){
+			if ((settings.currentImage == 0 && index == -1) || (settings.currentImage == settings.lastImage && index == 1)) {
 				return false;
-			}
-			else{
-				$('#wireboxDescription').css('visibility','hidden');
-				$('#wireboxImage').fadeTo(settings.fadeOutImageduration, 0, function(){
-					load(settings.currentImage + parseInt(signe), false);
+			} else {
+				$('#wireboxTitle, #wireboxCounter').fadeOut(settings.fadeOutImageduration);
+				$('#wireboxClose').fadeOut(settings.fadeOutImageduration);
+				$('#wireboxImage').fadeTo(settings.fadeOutImageduration, 0, function() {
+					load(settings.currentImage + parseInt(index), false);
 				});
 			}
 		}
-		function Wirebox(height,width,resize){
-
-			if(resize){
+		function Wirebox(imgHeight, imgWidth, resize) {
+			if (resize) {
 				$('#wireboxContainer, #wireboxContent, #wireboxImage').stop(true,false).css({'overflow':'visible'});
-				$('#wireboxImage').animate({
-					'height' : height+'px',
-					'width' : width+'px'
-				},settings.fadeInImageduration);
+				$('#wireboxImage')
+					.animate({
+						'height' : imgHeight + 'px',
+						'width' : imgWidth + 'px'
+					}, settings.fadeInImageduration);
 			}
-			$('#wireboxContainer').animate({
-					'height' : height,
-					'width' : width
-			},settings.fadeInImageduration);
-			$('#wireboxContent').animate({
-				'height' : height,
-				'width' : width,
-				'marginLeft' : -width/2,
-				'marginTop' : -(height)/2
-			},settings.fadeInImageduration, 'swing', function(){
-				$('#wireboxImage').fadeTo(settings.fadeInImageduration, 1).height(height).width(width);
-				if(!resize)
-				{
-					arrowsManaging();
-					//$('#wireboxDescription').fadeTo(settings.fadeInImageduration,1); making a weird bug with firefox 17
-					$('#wireboxDescription').css('visibility','visible');
+			$('#wireboxContainer')
+				.animate({
+					'height' : imgHeight,
+					'width' : imgWidth
+				}, settings.fadeInImageduration);
+			$('#wireboxContent')
+				.animate({
+					'height' : imgHeight,
+					'width' : imgWidth,
+					'marginLeft' : -imgWidth / 2,
+					'marginTop' : -imgHeight / 2
+				}, settings.fadeInImageduration, 'swing', function() {
+					$('#wireboxImage')
+						.fadeTo(settings.fadeInImageduration, 1)
+						.height(imgHeight)
+						.width(imgWidth);
+				if(!resize) {
+					arrows();
+					$('#wireboxTitle, #wireboxCounter').fadeIn(settings.fadeInImageduration);
 					$('#wireboxClose').fadeIn(settings.fadeInImageduration);
 				}
-			}).
-			css('overflow', 'visible');
+			}).css('overflow', 'visible');
 		}
-		function arrowsManaging(){
-			if(settings.linkImages){
-				var what = ['wireboxRight','wireboxLeft'];
-				for(var i=0; i < what.length; i++){
-					hide = false;
-					if(what[i] == 'wireboxRight' && settings.currentImage == settings.lastImage){
-						hide = true;
-						$('#'+what[i]).fadeOut(300);
-					}
-					else if(what[i] == 'wireboxLeft' && settings.currentImage == 0){
-						hide = true;
-						$('#'+what[i]).fadeOut(300);
-					}
-					if(!hide){
-						$('#'+what[i]).fadeIn(settings.fadeOutImageduration);
-					}
+		function arrows(){
+			var arrows = ['wireboxRight','wireboxLeft'];
+			for (var i=0; i < arrows.length; i++) {
+				hide = false;
+				if (arrows[i] == 'wireboxRight' && settings.currentImage == settings.lastImage) {
+					hide = true;
+					$('#'+arrows[i]).fadeOut(300);
+				} else if(arrows[i] == 'wireboxLeft' && settings.currentImage == 0) {
+					hide = true;
+					$('#'+arrows[i]).fadeOut(300);
+				}
+				if (!hide) {
+					$('#'+arrows[i]).fadeIn(settings.fadeOutImageduration);
 				}
 			}
 		}
-		function preload(){
-			if(settings.currentImage !== settings.lastImage){
-				i = new Image;
-				z = settings.currentImage + 1;
-				i.src = images[settings.setIndex][z]['location'];
+		function preload() {
+			if (settings.currentImage !== settings.lastImage) {
+				image = new Image;
+				advance = settings.currentImage + 1;
+				image.src = images[settings.setIndex][advance]['location'];
 			}
 		}
-		function upadteDescription(){
+		function description() {
 			var current = settings.currentImage + 1;
 			var last = settings.lastImage + 1;
 			$('#wireboxTitle').html(images[settings.setIndex][settings.currentImage]['caption']);
-			$('#wireboxCounter').html(settings.name+settings.setTitle+current+settings.separator+last);
+			$('#wireboxCounter').html(settings.prefix + ' ' + current + ' ' + settings.separator + ' ' + last);
 		}
-		function isSet(variable,defaultValue){
-			// return variable === undefined ? defaultValue : variable; ?
+		function isSet(variable,defaultValue) {
 			if (variable === undefined) {
 				return defaultValue;
-			}
-			else{
+			} else {
 				return variable;
 			}
 		}
-		function iWantThePerfectImageSize(himg,limg){
-			//28% = 14% + 14% margin
-			var lblock = limg + (limg*28/100);
-			var heightDescAndClose = $('#wireboxDescription').height()+$('#wireboxClose').height();
-			var hblock = himg + heightDescAndClose;
-			var k = limg/himg;
-			var kk = himg/limg;
-			if(settings.container.get(0).nodeName.toLowerCase() == 'body'){
+		function resizeImage(imgHeight, imgWidth){
+			var widthRatio = imgWidth + (imgWidth * 28 / 100);
+			var elements = $('#wireboxClose').height() + $('#wireboxCounter').height();
+			var heightRatio = imgHeight + elements + 50;
+			var vertical = imgWidth/imgHeight;
+			var horizontal = imgHeight/imgWidth;
+			if (settings.container.get(0).nodeName.toLowerCase() == 'body') {
 				windowHeight = $(window).height();
 				windowWidth = $(window).width();
-			}
-			else{
+			} else {
 				windowHeight = settings.container.height();
 				windowWidth = settings.container.width();
 			}
 			notFitting = true;
-				while (notFitting){
-				var lblock = limg + (limg*28/100);
-				var hblock = himg + heightDescAndClose;
-					if(lblock > windowWidth){
-						limg = windowWidth*100/128;
-						
-						himg = kk * limg;
-					}else if(hblock > windowHeight){
-						himg = (windowHeight - heightDescAndClose);
-						limg = k * himg;
-					}else{
+				while (notFitting) {
+				var widthRatio = imgWidth + (imgWidth * 28 / 100);
+				var heightRatio = imgHeight + elements + 50;
+					if (widthRatio > windowWidth) {
+						imgWidth = windowWidth * 100 / 128;
+						imgHeight = horizontal * imgWidth;
+					} else if (heightRatio > windowHeight) {
+						imgHeight = windowHeight - elements - 50;
+						imgWidth = vertical * imgHeight;
+					} else {
 						notFitting = false;
 					};
 				};
 			return {
-				width:limg,
-				height:himg
+				width : imgWidth,
+				height : imgHeight
 			};
 
 		}
-		function clear(){
+		function clear() {
 			$('#wireboxOverlay').remove();
 			$('#wireboxContent').remove();
 		}
-		function close(){
-			$('#wireboxOverlay').fadeOut(500, function(){$('#wireboxOverlay').remove()});
-			$('#wireboxContent').fadeOut(200, function(){$('#wireboxContent').remove()});
+		function close() {
+			$('#wireboxOverlay').fadeOut(settings.fadeOutOverlayDuration, function(){$('#wireboxOverlay').remove()});
+			$('#wireboxContent').fadeOut(settings.fadeOutOverlayDuration, function(){$('#wireboxContent').remove()});
 			settings.currentImage = 0;
 		}
 	
--- readme.md
+++ readme.md
@@ -14,5 +14,45 @@ Responsive lightbox for image galleries (Fancybox alternative)
 
 - Wirebox module automatically replaces Fancybox module for images / image galleries.
 
+## Options
+
+- Click on the Wirebox module on Modules page and override options defaults
+
+### Overlay Opacity
+
+- Opacity of the overlay (from 0 to 1; default = 0.9)
+
+### Overlay Color
+
+- Color of the overlay
+
+### Overlay Close
+
+- Toggle if clicking the overlay should close Wirebox
+
+### Overlay FadeIn Speed
+
+- Speed of overlay fadeIn effect, in milliseconds
+
+### Overlay FadeOut Speed
+
+- Speed of overlay fadeOut effect, in milliseconds
+
+### Image FadeIn Speed
+
+- Speed of image fadeIn effect, in milliseconds
+
+### Image FadeOut Speed
+
+- Speed of image fadeOut effect, in milliseconds
+
+### Counter Prefix
+
+- Sets the counter prefix; default = "Image"
+
+### Counter Separator
+
+- Sets the counter separator; default = "/"
+
 ------
 Copyright 2013 Nikola Vidoni
